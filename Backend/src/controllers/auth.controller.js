0b88a52e015f8e1414e8b2454038fdeddc224119
saim16 <12345saimart@gmail.com>
2026-08-11 19:27:43 +0530

    updated cookies

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -196,9 +196,11 @@ async function loginUser(req, res) {
         role: getUser.role
     }, process.env.JWT_SECRET, { expiresIn: '3d' });
 
-    res.cookie("token", token, {
+    res.cookie('token', token, {
         httpOnly: true,
-        sameSite: 'lax',
+        secure: true,
+        sameSite: 'none',
+        maxAge: 24 * 60 * 60 * 1000
     });
 
     res.status(200).json({

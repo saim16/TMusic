fee897bdf4cbe8050ad8e1d476611fff05b2d988
saim16 <12345saimart@gmail.com>
2026-08-11 20:27:52 +0530

    updated cookies - 4

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -150,7 +150,6 @@ async function registerUser(req, res) {
 
     res.cookie('token', token, {
         httpOnly: true,
-        secure: true,
         sameSite: 'none',
         maxAge: 24 * 60 * 60 * 1000
     });
@@ -200,7 +199,6 @@ async function loginUser(req, res) {
 
     res.cookie('token', token, {
         httpOnly: true,
-        secure: true,
         sameSite: 'none',
         maxAge: 24 * 60 * 60 * 1000
     });
